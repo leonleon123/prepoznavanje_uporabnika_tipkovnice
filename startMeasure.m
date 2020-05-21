2a0db78bbb6a8063a22f777c6b6b3578760607d3
ar43 <47793453+ar43@users.noreply.github.com>
2020-05-21 20:44:08 +0200

    fix delta calculation logic
    -renamed some variables
    -delta for first 2 inputs gets calculated correctly
    -delta now gets calculated between 2 valid inputs

--- startMeasure.m
+++ startMeasure.m
@@ -1,5 +1,5 @@
 function [timesAverage, n] = startMeasure()
-    s=0;
+    lastTime=0;
     keys = [65:90, 48:57, 191, 187, 8, 9, 20, 160, 188, 190, 189, 161, 32];
     findKey = @(a) find(keys==a);
     times = zeros(length(keys), length(keys));
@@ -12,21 +12,21 @@ function [timesAverage, n] = startMeasure()
     while lastKey ~= 27
         [pressed, keyCode] = KbQueueCheck();
         if pressed
-            a = toc;
-            delta = a-s;
-            s = a;
             keyCodeNum = find(keyCode);
             keyCodeNum = keyCodeNum(1);
-            fprintf('from:%d to:%d delta:%f\n',lastKey,keyCodeNum,delta);
-            if lastKey >= 0
-                i = findKey(lastKey);
-                j = findKey(keyCodeNum);
-                if ~isempty(i) && ~isempty(j)
+            i = findKey(lastKey);
+            j = findKey(keyCodeNum);
+            if ~isempty(j)
+                currentTime = toc;
+                delta = currentTime-lastTime;
+                lastTime = currentTime;
+                if lastKey >= 0 && ~isempty(i)
+                    fprintf('from:%d to:%d delta:%f\n',lastKey,keyCodeNum,delta);
                     times(i,j) = times(i,j) + delta;
                     n(i,j) = n(i,j) + 1;
                 end
+                lastKey = keyCodeNum;
             end
-            lastKey = keyCodeNum;
         end
     end
     timesAverage = times./(n+(n==0));
