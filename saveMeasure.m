46fae1e5ab2de8c6368ef6d293f7be791e627d34
leonleon123 <lele.slav@gmail.com>
2020-05-22 18:47:51 +0200

    automatically creates folders

--- saveMeasure.m
+++ saveMeasure.m
@@ -1,5 +1,11 @@
-function saveMeasure(name, i)
-    path = sprintf('%s/%d.mat', name, i);
+function saveMeasure(user, text, i)
+    if exist(sprintf('tests/%s', user)) ~= 7
+        mkdir(sprintf('tests/%s', user));
+    end
+    if exist(sprintf('tests/%s/%s', user, text)) ~= 7
+        mkdir(sprintf('tests/%s/%s', user, text));
+    end
+    path = sprintf('tests/%s/%s/%d.mat', user, text, i);
     X = startMeasure();
     save(path, 'X');
 end
