31b309cb6b7cb7ceee99a76697fd5dc76aaaaac3
leonleon123 <lele.slav@gmail.com>
2020-06-07 18:23:50 +0200

    latex update

--- main.m
+++ main.m
@@ -10,8 +10,8 @@ function main()
     averageMatrix = startMeasure();
     [name, minNorm, x] = mainCalculate(averageMatrix(:));
     fprintf('minimal norm: %f\n', minNorm);
-    % fprintf('average time from space to shift: %f\n', averageMatrix(47,42));
-    % fprintf('average time from space to capslock: %f\n', averageMatrix(47,41));
+    fprintf('average time from space to shift: %f\n', averageMatrix(47,42));
+    fprintf('average time from space to capslock: %f\n', averageMatrix(47,41));
     tol = 0.02;
     if(x < tol), fprintf('could not identify\n');
     else, fprintf('we know who you are, %s >:)\n', name);
